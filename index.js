129b881b00693912c914e9525f3e02573c0108d7
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-06-02 15:09:44 -0300

    refactor: add class list toggle

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 function toggleMode() {
   const body = document.querySelector("body");
-
-  if (body.classList.contains("light")) {
-    body.classList.remove("light");
-  } else {
-    body.classList.add("light");
-  }
+  body.classList.toggle("light");
 }
