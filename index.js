bdcf2d96552083d4a17fce8e83cb6232126e6dd7
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-06-02 16:51:11 -0300

    feat: alter switch button icon when click switch button

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const body = document.querySelector("body");
 const switchButton = document.querySelector("#switch button");
+const switchButtonIcon = document.querySelector("#switch button #icon");
 
 const toggleMode = () => {
   body.classList.toggle("light");
+
+  if (body.classList.contains("light")) {
+    switchButtonIcon.setAttribute("name", "sunny-outline");
+  } else {
+    switchButtonIcon.setAttribute("name", "moon-outline");
+  }
 };
 
 switchButton.addEventListener("click", toggleMode);
