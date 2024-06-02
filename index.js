cbf71c386bf7707a0b91136dcb79d5089d106ec9
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-06-02 15:14:05 -0300

    refactor: switch button add event listener click toggle mode

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function toggleMode() {
-  const body = document.querySelector("body");
+const body = document.querySelector("body");
+const switchButton = document.querySelector("#switch button");
+
+const toggleMode = () => {
   body.classList.toggle("light");
-}
+};
+
+switchButton.addEventListener("click", toggleMode);
